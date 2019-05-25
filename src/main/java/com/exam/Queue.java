9647832b8328646647316728fc4a4f6fc4e2d1d3
Nisar Adappadathil <adanisar@amazon.com>
2019-05-25 12:05:19 +0530

    added exception

--- src/main/java/com/exam/Queue.java
+++ src/main/java/com/exam/Queue.java
@@ -4,9 +4,9 @@ public interface Queue<T> {
     public Queue<T> enQueue(T t);
 
     //Removes the element at the beginning of the immutable queue,and returns the new queue.
-    public Queue<T> deQueue();
+    public Queue<T> deQueue() throws Exception;
 
-    public T head();
+    public T head() throws Exception;
 
     public boolean isEmpty();
 }
